1ff071e87fc65cb4025de239ede2c557fefdc28c
John Stovin <john.d.stovin@gmail.com>
2018-07-23 23:52:51 +0100

    Fix display

--- src/Scale/View.fs
+++ src/Scale/View.fs
@@ -4,12 +4,13 @@ open Theory.Tab
 open Theory.Scale
 
 open Fable.Helpers.React
+open Fable.Helpers.React.Props
 
 let root model dispatch =
   let frets = (drawTab true 5 openD)
-  let cMaj = octave C |> major
+  let cMaj = octave C |> major |> triad
   let lines = drawTab true 5 openD cMaj |> Seq.map (fun x -> [str x; br []]) |> Seq.collect id |> Seq.toList
 
   div
-    [ ]
+    [ ClassName "fretboard" ]
     lines
